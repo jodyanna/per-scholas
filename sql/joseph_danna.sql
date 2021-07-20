0fd8bbc1b90b2334c0b771903c3f39213f6cb2bb
joseph danna <jodyanna14.15@gmail.com>
2021-07-19 23:12:02 -0500

    completed sql assignment simple queries

---
+++ sql/joseph_danna.sql
@@ -0,0 +1,36 @@
+-- MySQL Hands-On #1: Simple Queries
+
+-- 1
+select productName as "Name", productLine as "Product Line", buyPrice as "Buy Price"
+from products
+order by buyPrice desc;
+
+-- 2
+select contactFirstName as "First Name", contactLastName as "Last Name", city as "City"
+from customers
+where country="Germany"
+order by contactLastName asc;
+
+--3
+select distinct status
+from orders
+order by status asc;
+
+--4
+select *
+from payments
+where paymentDate >= "20050101"
+order by paymentDate asc;
+
+--5
+select lastName, firstName, email, jobTitle
+from employees
+join offices on employees.officeCode=offices.officeCode
+where offices.city="San Francisco"
+order by lastName asc;
+
+--6
+select productName, productLine, productScale, productVendor
+from products
+where productLine="Classic Cars" or productLine="Vintage Cars"
+order by productLine desc, productName asc;
